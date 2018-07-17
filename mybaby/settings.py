7d8287cf5d9486190b2bd87227bc314cb9afc785
wonjin911 <wonjin911@24dcaea7d349>
2018-07-17 06:02:12 +0000

    static fixed

--- mybaby/settings.py
+++ mybaby/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-		'bootstrap3',
+    'bootstrap3',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -119,9 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
